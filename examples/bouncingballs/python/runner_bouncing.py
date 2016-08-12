9a2fb97510bf3027c1fea886abeb1163ee5cfc70
Simon Van Mierlo <simon.vanmierlo@uantwerpen.be>
2016-08-12 13:43:19 +0200

    better bouncing balls example

---
+++ examples/bouncingballs/python/runner_bouncing.py
@@ -0,0 +1,19 @@
+'''
+Created on 27-jul.-2014
+
+@author: Simon
+'''
+
+import Tkinter as tk
+import target_py.bouncing as target
+from sccd.runtime.statecharts_core import Event
+from sccd.runtime.tkinter_eventloop import *
+from widget import Widget
+
+if __name__ == '__main__':
+    window = tk.Tk()
+    window.withdraw()
+    controller = target.Controller(window, TkEventLoop(window))
+    Widget.controller = controller
+    controller.start()
+    window.mainloop()
